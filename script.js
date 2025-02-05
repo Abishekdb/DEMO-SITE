e0b9668f5918403aa9ab87d1a33701e79c279176
Abishekdb <dbtelegrambots@gmail.com>
2025-02-05 17:30:11 +0530

    website

---
+++ script.js
@@ -0,0 +1,6 @@
+// Day/Night Mode Toggle
+const toggleButton = document.getElementById('toggle-theme');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  toggleButton.textContent = document.body.classList.contains('dark') ? '☀️' : '????';
+});
