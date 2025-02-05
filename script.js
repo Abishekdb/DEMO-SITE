c62c4d38c4bf654a47481e8381eeacbaf7320d84
Abishekdb <dbtelegrambots@gmail.com>
2025-02-05 17:35:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 const toggleButton = document.getElementById('toggle-theme');
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark');
-  toggleButton.textContent = document.body.classList.contains('dark') ? '☀️' : '????';
+  toggleButton.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
 });
